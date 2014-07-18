9893b86c2ce3931d0d3586d63241f0d814e006c0
tpmetiv1 <tpmetiv1@svsu.edu>
2014-07-18 19:10:42 -0400

    Updated Profile Layout
    I've added some cosmetic additions, as well as a Bootstrap modal. I did
    away with the "profile.js" file, as I didn't see the point of an
    external dependency that contained only two functions. I just added the
    script to the bottom of the PHP file.

--- svsuitthing/profile.php
+++ svsuitthing/profile.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ITS Support Center - REC/HHS</title>
-    <link href="css/bootstrap.min.css" rel="stylesheet"/>
-    <link href="css/style.css" rel="stylesheet"/>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>ITS Support Center - REC/HHS</title>
+	<link href="css/bootstrap.min.css" rel="stylesheet"/>
+	<link href="css/jasny-bootstrap.min.css" rel="stylesheet">
+	<link href="css/bootstrap-datetimepicker.min.css" rel="stylesheet"/>
+	<link href="css/style.css" rel="stylesheet"/>
 </head>
 <body>
 	<?php include 'navbar.php'; ?>
@@ -16,18 +18,31 @@
 			<li class="active"><a href="#ryan" data-toggle="tab">Ryan Bickham</a></li>
 			<li><a href="#eric" data-toggle="tab">Eric DeSmet</a></li>
 		</ul>
+
+		<!-- This divider contains recent cosmetic additions -->
 		<div class="tab-content">
 			<div class="tab-pane fade active in" id="ryan">
 				<div class="row">
 					<div class="col-md-2">
 						<img src="images/ryan.jpg" class="profile img-responsive img-rounded" />
 						<ul class="list-unstyled profile_info">
+							<li id="editStudent"><span class="glyphicon glyphicon-pencil"></span><a href="#">Edit Profile</a></li><br />
 							<li class="tooltipped" data-toggle="tooltip" data-placement="left" title="Start Date"><span class="glyphicon glyphicon-calendar"></span>Started August 21, 2011</li>
 							<li class="tooltipped" data-toggle="tooltip" data-placement="left" title="Email"><span class="glyphicon glyphicon-envelope"></span><a href="mailto:rcbickha@svsu.edu">rcbickha@svsu.edu</a></li>
 							<li class="tooltipped" data-toggle="tooltip" data-placement="left" title="Major"><span class="glyphicon glyphicon-star"></span>Computer Science</li>
 							<li class="tooltipped" data-toggle="tooltip" data-placement="left" title="Minor"><span class="glyphicon glyphicon-star-empty"></span>Mathematics</li>
 						</ul>
+						<div class="panel panel-primary">
+							<div class="panel-heading">
+								<h3 class="panel-title">About Me</h3>
+							</div>
+							<div class="panel-body">
+								This is where we'll type interesting things about ourselves.
+							</div>
+						</div>
 					</div>
+					<!-- End of additions -->
+
 					<div class="col-md-9">
 						<div class="panel-group" id="badge_accordian_ryan">
 							<div class="panel panel-success">
@@ -127,8 +142,63 @@
 			</div>
 		</div>
 	</div>
+
+	<!-- Added Modal -->
+	<div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true" class="modal_x">&times;</span><span class="sr-only">Close</span></button>
+					<h4 class="modal-title" id="myModalLabel">Edit Profile</h4>
+				</div>
+				<div class="modal-body">
+					<form class="form-horizontal" id="studentForm" method="post">
+						<div class="form-group">
+							<div class="fileinput fileinput-new" data-provides="fileinput">
+								<div class="fileinput-preview thumbnail" data-trigger="fileinput"></div>
+								<div>
+								</div>
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="expGradDate" class="col-md-4 control-label">Expected Graduation Date</label>
+							<div class="col-md-7">
+								<input type="text" class="form-control" id="expGradDate" name="expGradDate">
+							</div>
+						</div> 
+						<div class="form-group">
+							<label for="aboutMe" class="col-md-4 control-label">About Me</label>
+							<div class="col-md-7">
+								<textarea class="form-control" rows="3" id="aboutMe" name="aboutMe" placeholder="Edit About Me"></textarea>
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="majorField" class="col-md-4 control-label">Major</label>
+							<div class="col-md-5">
+								<input type="text" class="form-control" id="majorField" name="majorField" placeholder="Edit Major">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="minorField" class="col-md-4 control-label">Minor</label>
+							<div class="col-md-5">
+								<input type="text" class="form-control" id="minorField" name="minorField" placeholder="Edit Minor">
+							</div>
+						</div>       				
+					</form>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+					<button type="button" class="btn btn-primary" id="adminFormSubmit">Save</button>
+				</div>
+			</div>
+		</div>
+	</div>
+	
 	<script src="scripts/jquery-1.11.0.min.js"></script>
 	<script src="scripts/bootstrap.min.js"></script>
+	<script src="scripts/moment.min.js"></script>
+	<script src="scripts/bootstrap-datetimepicker.js"></script>
+
 	<script>
 		$(function() {
 			$('.tooltipped').tooltip();
@@ -142,6 +212,21 @@
 				}
 			});
 		});
+
+		// Added JQuery
+		$(function() {
+			$('#editStudent').click(function(){
+				$('#editModal').modal('show');
+			});
+		});
+
+		$(function() {
+			$('#expGradDate').datetimepicker({
+				pickTime: false
+			});
+		});
+		//
 	</script>
+
 </body>
 </html>
